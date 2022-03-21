2850283006cbe0a21dc1e73d0fe2c8e49101da9a
Attama Pascal <Attamapascalpedro@gmail.com>
2022-03-21 08:05:20 +0100

    Initial commit

---
+++ mpl.demo.py
@@ -0,0 +1,42 @@
+
+"""from matplotlib import pyplot as plt
+
+dev_x = [25, 26, 27,28, 29, 30, 31, 32, 33, 34, 35]
+
+dev_y = [38496, 42000, 46752, 49320, 53200, 56000, 62316, 64928, 67317, 68748, 73752]
+
+plt.plot(dev_x, dev_y)
+
+plt.xlabel('Ages')
+plt.ylabel('Median Salary (USD')
+plt.title('Median Salary(USD) by Age')
+
+plt.show()"""
+
+
+
+
+
+
+
+from matplotlib import pyplot as plt
+
+ages_x = [25, 26, 27,28, 29, 30, 31, 32, 33, 34, 35]
+
+dev_y = [38496, 42000, 46752, 49320, 53200, 56000, 62316, 64928, 67317, 68748, 73752]
+
+plt.plot(ages_x, dev_y, color='y', linestyle='--', label='All Devs')
+
+py_dev_y = [45372, 48876, 53850, 57287, 63016, 65998, 70003, 70000, 71496, 75370, 83640]
+
+plt.plot(ages_x, py_dev_y, color='b', linestyle='-.', label='Python')
+
+plt.xlabel('Ages')
+plt.ylabel('Median Salary (USD')
+plt.title('Median Salary(USD) by Age')
+
+plt.grid()
+
+plt.legend()
+
+plt.show()
